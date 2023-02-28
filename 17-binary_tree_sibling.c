352845ce69117c80c5da3c3abe8491378c7fe702
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-02-28 19:55:36 +0300

    binary trees - node sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,7 +8,7 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (node->parent)
+	if (node && node->parent)
 	{
 		if ((node->parent->left) == node)
 			return (node->parent->right);
