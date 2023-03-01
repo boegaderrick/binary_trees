ee56daf051a9407ec1bf4cf37ca089fa9099a298
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-03-01 16:35:09 +0300

    binary trees - tree perfection check

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,42 @@
+#include "binary_trees.h"
+
+size_t height(const binary_tree_t *tree, bool *perfect);
+
+/**
+ * binary_tree_is_perfect - checks if binary tree is perfect
+ * @tree: subject tree
+ *
+ * Return: 1 if tree is balanced, 0 otherwise
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	bool perfect = true;
+
+	if (tree == NULL)
+		return (0);
+
+	height(tree, &perfect);
+	return (perfect ? 1 : 0);
+}
+
+/**
+ * height - calculates height of a binary tree
+ * @tree: subject tree
+ * @perfect: boolean pointer to record whether or not tree is perfect
+ *
+ * Return: height of the tree or 0 if if tree is NULL
+ */
+size_t height(const binary_tree_t *tree, bool *perfect)
+{
+	size_t right = 0, left = 0;
+
+	if (tree == NULL || (!tree->left && !tree->right))
+		return (0);
+
+	left = height(tree->left, perfect);
+	right = height(tree->right, perfect);
+	if (left != right)
+		(*perfect) = false;
+
+	return (1 + (left > right ? left : right));
+}
--- binary_trees.h
+++ binary_trees.h
@@ -4,6 +4,8 @@
 /* HEADER FILES */
 #include <stdlib.h>
 #include <stddef.h>
+#include <stdbool.h>
+#include <stdio.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -44,6 +46,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 size_t binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
